d5f99b85e632f798123c9692593c75f709b9e0d4
Guo Liyong <guonwpu@qq.com>
2021-05-21 17:35:24 +0800

    rename decode entrance

--- egs/librispeech/asr/simple_v1/ctc_inference.sh
+++ egs/librispeech/asr/simple_v1/ctc_inference.sh
@@ -1,5 +1,6 @@
 export CUDA_VISIBLE_DEVICES=2
-python3 local_snowfall/nnlm_asr_inference.py \
+# python3 local_snowfall/nnlm_asr_inference.py \
+python3  tokenizer_ctc_att_transformer_decode.py \
         --asr_train_config exp/kamo-naoyuki/librispeech_asr_train_asr_conformer6_n_fft512_hop_length256_raw_en_bpe5000_scheduler_confwarmup_steps40000_optim_conflr0.0025_sp_valid.acc.ave/config.yaml \
         --asr_model_file exp/kamo-naoyuki/librispeech_asr_train_asr_conformer6_n_fft512_hop_length256_raw_en_bpe5000_scheduler_confwarmup_steps40000_optim_conflr0.0025_sp_valid.acc.ave/valid.acc.ave_10best.pth \
         --lm_train_config exp/lm_train_lm_transformer2_en_bpe5000/config.yaml \
